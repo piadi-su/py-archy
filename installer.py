83988d97d9b4d0edd03986c011068e1169e7ba74
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-03-08 18:23:40 +0100

    create repo, first test commit

---
+++ installer.py
@@ -0,0 +1,15 @@
+import time
+import os
+import subprocess
+
+
+def menu():
+    print("""
+======================================
+=|  PIADI: ARCH-INSTALLATION SCRIPT |=
+======================================
+github: https://github.com/piadi-su
+
+    """)
+
+menu()
